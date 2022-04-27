6a953230e8788d8ea9770ac8f6dbbf553e5f5250
Shashank-Karna <72338360+Shashank-Karna@users.noreply.github.com>
2022-04-27 05:45:06 +0530

    Update DFID.py

--- DFID.py
+++ DFID.py
@@ -1,63 +1,41 @@
-# DFID
+#DFID
 
-from collections import defaultdict
-  
-# This class represents a directed graph using adjacency
-# list representation
-class Graph:
-  
-    def __init__(self,vertices):
-  
-        # No. of vertices
-        self.V = vertices
-  
-        # default dictionary to store graph
-        self.graph = defaultdict(list)
-  
-    # function to add an edge to graph
-    def addEdge(self,u,v):
-        self.graph[u].append(v)
-  
-    # A function to perform a Depth-Limited search
-    # from given source 'src'
-    def DLS(self,src,target,maxDepth):
-  
-        if src == target : return True
-  
-        # If reached the maximum depth, stop recursing.
-        if maxDepth <= 0 : return False
-  
-        # Recur for all the vertices adjacent to this vertex
-        for i in self.graph[src]:
-                if(self.DLS(i,target,maxDepth-1)):
-                    return True
+graph = {
+    'A': ['B', 'C'],
+    'B': ['D','E'],
+    "C": ['G'],
+    'D': [],
+    'E': ['F'],
+    'G': [],
+    'F':[]
+}
+
+path = list()
+
+def DFS(currentNode,destination,graph,maxDepth,curList):
+    print("Checking for destination",currentNode)
+    curList.append(currentNode)
+    if currentNode==destination:
+        return True
+    if maxDepth<=0:
+        path.append(curList)
         return False
-  
-    # IDDFS to search if target is reachable from v.
-    # It uses recursive DLS()
-    def IDDFS(self,src, target, maxDepth):
-  
-        # Repeatedly depth-limit search till the
-        # maximum depth
-        for i in range(maxDepth):
-            if (self.DLS(src, target, i)):
-                return True
-        return False
-  
-# Create a graph given in the above diagram
-g = Graph (7);
-g.addEdge(0, 1)
-g.addEdge(0, 2)
-g.addEdge(1, 3)
-g.addEdge(1, 4)
-g.addEdge(2, 5)
-g.addEdge(2, 6)
-  
-target = 6; maxDepth = 3; src = 0
-  
-if g.IDDFS(src, target, maxDepth) == True:
-    print ("Target is reachable from source " +
-        "within max depth.")
-else :
-    print ("Target is NOT reachable from source " +
-        "within max depth.")
+    for node in graph[currentNode]:
+        if DFS(node,destination,graph,maxDepth-1,curList):
+            return True
+        else:
+            curList.pop()
+    return False
+
+def iterativeDDFS(currentNode,destination,graph,maxDepth):
+    for i in range(maxDepth):
+        curList = list()
+        if DFS(currentNode,destination,graph,i,curList):
+            return True
+    return False
+
+if not iterativeDDFS('A','E',graph,4):
+    print("Path is not available")
+else:
+    print("A path exists")
+    print(path.pop())
